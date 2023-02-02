d74de6983f4324c249c19a3f2d4be24bdc18a614
Jartisick <Jartisick@gmail.com>
2023-02-02 11:24:02 -0400

    about done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar";
 import { Inter } from "@next/font/google";
 import Main from "@/components/Main";
+import About from "@/components/About";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <NavBar />
       </main>
       <Main />
+      <About />
     </div>
   );
 }
